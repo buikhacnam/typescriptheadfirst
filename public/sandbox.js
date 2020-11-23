1c5a91351c4c17fc5903f7925c7f87e2ae5be4a1
Bui Nam <buikhacnam11@gmail.com>
2020-11-23 15:15:45 +0700

    function

--- public/sandbox.js
+++ public/sandbox.js
@@ -42,3 +42,90 @@ anything = true;
 anything = "ok";
 var anyArray = [];
 var anyObject;
+// Function
+var greet;
+// you cannot do this:
+// greet = "hello";
+// you can do this:
+greet = function () {
+    console.log("hello");
+};
+var add = function (a, b) {
+    console.log(a + b);
+};
+//so you can only do this:
+add(1, 2);
+var add1 = function (a, b, c) {
+    console.log(a + b);
+};
+//so you can only do this:
+add1(1, 2, 3);
+add1(1, 2, "hello");
+//optional parameters
+var add3 = function (a, b, c) {
+    console.log(c);
+};
+// c = 3;
+add3(1, 2, 3);
+// c = "undifined"
+add3(1, 2);
+//default parameters
+var add4 = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(c);
+};
+// c = 3;
+add3(1, 2, 3);
+// c = 10
+add3(1, 2);
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
+// you cannot do this:
+//result = "hehe"
+//if you want to know quicky what type of value will be return, do this:
+var minus1 = function (a, b) {
+    return a + b;
+};
+var result1 = minus1(10, 7);
+//if you want to know quicky that the function will return nothing, use void:
+var minus2 = function (a, b) {
+    if (b === void 0) { b = 10; }
+    console.log(a + b);
+};
+var result2 = minus2(10);
+//class9: Type Aliases:
+var logDetails = function (uid, item) {
+    //do something here
+};
+var greet1 = function (user) {
+    // do something here
+};
+var greet2 = function (user) {
+    // do something here
+};
+var greet3 = function (user) {
+    // do something here
+};
+//Signature Function:
+// example 1
+var gree;
+gree = function (name, greeting) {
+    console.log(name + " says " + greeting);
+};
+// example 2
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+// example 3
+var logDetail;
+logDetail = function (ninja) {
+    console.log(ninja.name + " is " + ninja.age + " years old");
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -25,7 +25,6 @@ console.log(circ(20)); //good job.
 
 
 //About array:
-
 let names = ["drogba", "lampard", "terry"];
 
 // names.push(11) error
@@ -64,6 +63,118 @@ let anyArray: any[] = [];
 let anyObject: {name: any, age: any};
 
 
+// Function
+let greet: Function;
+// you cannot do this:
+// greet = "hello";
+
+// you can do this:
+greet = () => {
+    console.log("hello");
+}
+
+const add = (a:number, b:number) => {
+    console.log(a + b);
+}
+//so you can only do this:
+add(1, 2);
+
+const add1 = (a:number, b:number, c: number|string) => {
+    console.log(a + b);
+}
+//so you can only do this:
+add1(1, 2, 3); 
+add1(1, 2, "hello");
+
+//optional parameters
+const add3 = (a: number, b: number, c?: number) => {
+    console.log(c);
+}
+// c = 3;
+add3(1, 2, 3);
+
+// c = "undifined"
+add3(1, 2);
+
+//default parameters
+const add4 = (a: number, b: number, c: number = 10) => {
+    console.log(c);
+}
+// c = 3;
+add3(1, 2, 3);
+
+// c = 10
+add3(1, 2);
 
 
+const minus = (a: number, b: number) => {
+    return a + b;
+  }
+  
+  let result = minus(10,7);
+// you cannot do this:
+//result = "hehe"
 
+//if you want to know quicky what type of value will be return, do this:
+const minus1 = (a: number, b: number): number => {
+    return a + b;
+  }
+  
+  let result1 = minus1(10,7);
+
+
+//if you want to know quicky that the function will return nothing, use void:
+const minus2 = (a: number, b: number = 10):void=> {
+    console.log(a + b);
+  }
+  
+  let result2 = minus2(10);
+
+
+  //class9: Type Aliases:
+
+  const logDetails = (uid: string | number, item: string) => {
+      //do something here
+  }
+
+
+const greet1 = (user: {name: string, uid: string | number}) => {
+    // do something here
+}
+
+type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum}
+
+const greet2 = (user: {name: string, uid: StringOrNum}) => {
+    // do something here
+}
+
+const greet3 = (user: objWithName) => {
+    // do something here
+}
+
+//Signature Function:
+// example 1
+let gree: (a: string, b: string) => void;
+
+gree = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+}
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
+}
+
+// example 3
+let logDetail: (obj: {name: string, age: number}) => void;
+
+logDetail = (ninja: {name: string, age: number}) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
