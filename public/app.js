95ee1909299eecfb5946dfa5bc09712b24580377
Bui Nam <buikhacnam11@gmail.com>
2020-11-25 09:12:46 +0700

    class and interface, app does the job

--- public/app.js
+++ public/app.js
@@ -30,6 +30,16 @@ greetPerson(me);
 //     }
 // }
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', "plumbing work", 200)
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
 // class Invoice {
 //     constructor(
 //       readonly client: string, 
@@ -40,16 +50,16 @@ import { Invoice } from './classes/Invoice.js';
 //       return `${this.client} owes £${this.amount} for ${this.details}`;
 //     }
 //   }
-const invOne = new Invoice('mario', 'work on the mario', 250);
-const invTwo = new Invoice('gomez', 'work on the gomez', 350);
-console.log(invOne, invTwo);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-console.log(invoices);
-invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+// const invOne = new Invoice('mario', 'work on the mario', 250);
+// const invTwo = new Invoice('gomez', 'work on the gomez', 350);
+//console.log(invOne, invTwo);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// console.log(invoices);
+// invoices.forEach(inv => {
+//     console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+// })
 // need a "!" in the end because anchor might be Null, so we declare that "!" to say that it's not Null
 const anchor = document.querySelector('a');
 //console.log(anchor.href);
@@ -61,7 +71,16 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,24 @@ greetPerson(me);
 //     }
 // }
 import { Invoice } from './classes/Invoice.js';
- // class Invoice {
+import { Payment } from './classes/Payment.js';
+import { HasFormatter} from './interfaces/HasFormatter.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', "plumbing work", 200)
+
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// console.log(docs);
+
+
+// class Invoice {
 //     constructor(
 //       readonly client: string, 
 //       private details: string, 
@@ -55,20 +72,22 @@ import { Invoice } from './classes/Invoice.js';
 //     }
 //   }
 
-const invOne = new Invoice('mario', 'work on the mario', 250);
-const invTwo = new Invoice('gomez', 'work on the gomez', 350);
+// const invOne = new Invoice('mario', 'work on the mario', 250);
+// const invTwo = new Invoice('gomez', 'work on the gomez', 350);
 
-console.log(invOne, invTwo);
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
 
-console.log(invoices);
+//console.log(invOne, invTwo);
 
-invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-})
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// console.log(invoices);
+
+// invoices.forEach(inv => {
+//     console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+// })
 
 
 // need a "!" in the end because anchor might be Null, so we declare that "!" to say that it's not Null
@@ -88,14 +107,18 @@ const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-  
-    console.log(
-      type.value, 
-      tofrom.value, 
-      details.value, 
-      amount.valueAsNumber
-    );
+
+    let doc: HasFormatter;
+    if (type.value === 'invoice') {
+      doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    } else {
+      doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    
+    list.render(doc, type.value, 'end');
 });
